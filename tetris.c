2aa60d21857ebd5833dcfa4a8f5ae43cac038a62
DQNEO <dqneoo@gmail.com>
2012-10-28 04:43:17 +0900

    spin out header file

--- tetris.c
+++ tetris.c
@@ -3,113 +3,7 @@
 #include <conio.h>
 #include <time.h>
 #include <string.h>
-
-#define KEY_LEFT   'j'
-#define KEY_RIGHT  'k'
-#define KEY_DOWN   ' '
-#define KEY_ROTATE 'x'
-
-#define INTERVAL 2000
-
-//関数プロトタイプ宣言
-void init();
-int  block_new();
-void block_operate();
-int  is_attached(int, int);
-void block_move(int, int);
-int  block_rotate();
-int  block_drop();
-int  block_lock();
-int  check_and_delete();
-void view_render();
-void view_gameover(); 
-void view_clear();
-int  rand_block_type();
-
-void copy_block(int src[4][4], int dst[4][4]);
-void copy_whole(int src[21][12], int dst[21][12]);
-
-//グローバル変数
-const int NUM_ROWS = 20;
-const int NUM_COLS = 10;
-
-char *tiles[3] = {
- "  ",
- "■",
- "□",
-};
-
-// 壁と固定済みブロック
-int background[21][12] ={0};
-// 画面データ。background[][]を背景としてその上にmyblock.pattern[][]を重ねたもの
-int view_data[21][12];
-
-//ブロックのr,c座標
-struct position {
-    int r;
-    int c;
-};
-
-//現在落下中のブロックを扱う構造体
-struct block {
-    //位置データ
-    struct position pos;
-    //形状データ
-    int pattern[4][4];
-};
-
-struct block myblock;
-
-
-int total_point = 0; //合計得点
-int add_points[5] = {0,100,300,500,1000}; //同時消しの加算ポイント
-
-
-//７種類のブロックの形状データ
-int block_patterns[7][4][4] = {
-    {
-        {0,1,0,0},
-        {0,1,0,0},
-        {0,1,0,0},
-        {0,1,0,0}
-    },
-    {
-        {0,0,0,0},
-        {0,1,1,0},
-        {0,1,0,0},
-        {0,1,0,0}
-    },
-    {
-        {0,0,1,0},
-        {0,1,1,0},
-        {0,1,0,0},
-        {0,0,0,0}
-    },
-    {
-        {0,1,0,0},
-        {0,1,1,0},
-        {0,0,1,0},
-        {0,0,0,0}
-    },
-    {
-        {0,0,0,0},
-        {0,1,0,0},
-        {1,1,1,0},
-        {0,0,0,0}
-    },
-    {
-        {0,0,0,0},
-        {0,1,1,0},
-        {0,1,1,0},
-        {0,0,0,0}
-    },
-    {
-        {0,0,0,0},
-        {0,1,1,0},
-        {0,0,1,0},
-        {0,0,1,0}
-    }
-};
+#include "tetris.h"
 
 int main()
 {
@@ -433,7 +327,7 @@ void view_render()
         strcat(output, offset);
         for(c = 0; c < NUM_COLS + 2; c++) {
             //1セル追加
-            cell = tiles[view_data[r][c]];
+            cell = (char *)tiles[view_data[r][c]];
             strcat(output, cell);
         }
         //1段終わり
